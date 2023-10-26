498eb72d8614ea8a0e8c144712880f16e32f90d0
rgsk <guptarahul70322@gmail.com>
2023-10-26 11:52:47 +0530

    first

---
+++ lib/implicit_examples/animated_color_pallete.dart
@@ -0,0 +1,63 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class AnimatedColorPalette extends StatefulWidget {
+  const AnimatedColorPalette({super.key});
+
+  @override
+  State<AnimatedColorPalette> createState() => _AnimatedColorPaletteState();
+}
+
+class _AnimatedColorPaletteState extends State<AnimatedColorPalette> {
+  List<Color> currentPalette = generateRandomPalette();
+
+  static List<Color> generateRandomPalette() {
+    final random = Random();
+    return List.generate(
+      5,
+      (_) => Color.fromRGBO(
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+        1,
+      ),
+    );
+  }
+
+  void regeneratePalette() {
+    setState(() {
+      currentPalette = generateRandomPalette();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Color Palette Generator'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (final color in currentPalette)
+              AnimatedContainer(
+                width: 100,
+                height: 100,
+                color: color,
+                margin: const EdgeInsets.all(8),
+                duration: const Duration(
+                  milliseconds: 500,
+                ),
+              ),
+            ElevatedButton(
+              onPressed: regeneratePalette,
+              child: const Text('Generate New Palette'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
